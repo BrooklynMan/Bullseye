18ab68dc8b110c048c28675736ef77b74b1e718d
adam mcbride <adam.mcbride@gmail.com>
2020-03-08 10:13:26 -0400

    progress so far

---
+++ Bullseye/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  Bullseye
+//
+//  Created by Adam McBride on 3/8/20.
+//  Copyright © 2020 Adam McBride Designs. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State var alertIsVisible: Bool = false
+    
+    var body: some View {
+        VStack {
+            Text("Welcome to my first app!")
+                .fontWeight(.semibold)
+                .foregroundColor(Color.green)
+            Button(action: {
+                print("Button pressed!")
+                self.alertIsVisible = true
+            }) {
+                Text(/*@START_MENU_TOKEN@*/"Hit Me!"/*@END_MENU_TOKEN@*/)
+            }
+            .alert(isPresented: $alertIsVisible) { () ->
+                Alert in
+                return Alert(
+                    title: Text("Hello there!"),
+                    message: Text("This is my first popup."),
+                    dismissButton: .default(Text("Awesome"))
+                )
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
